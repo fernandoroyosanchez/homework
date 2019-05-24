545d43c0e2b0fc840b55d725397764582f3dac25
root <root@ns3070194.ip-149-202-214.eu>
2019-05-24 09:58:01 +0200

    dos2unix to fix file, and improve display_usage help format

--- module1/start_containers.sh
+++ module1/start_containers.sh
@@ -2,26 +2,28 @@
 set -e
 
 #### Functions ###
-display_usage() { 
+display_usage() {
 	echo "This script must be run with root privileges and"
-	echo "docker should be installed!" 
-	echo "\nUsage:\n$0 <docker_image> <instances>\n" 
-	echo " <saved_image>\t\tThe image file to run" 	
-	echo " <instances>\t\tnumber of instances to start" 	
-	echo "\nExample: $0 centos 3 "
-} 
+	echo "docker should be installed!                     "
+	echo "  Usage:                                        "
+	echo "        $0 <docker_image> <instances>           "
+	echo "  <saved_image>     The image file to run       "
+	echo "  <instances>       number of instances to start"
+	echo "                                                "
+        echo "  Example: $0 centos 3                          "
+}
 
 # Check params
-if [  $# -ne 2 ] 
-then  
+if [  $# -ne 2 ]
+then
 	display_usage
 	exit 1
-fi 
+fi
 
 # Check docker is executable
-# 
+#
 docker images > /dev/null 2>&1; rc=$?;
-if [ $rc != 0 ]; then 
+if [ $rc != 0 ]; then
 	display_usage
 	exit 1
 fi
@@ -33,7 +35,7 @@ docker pull $1 > /dev/null
 # Generate simbolic link to namespace for containers
 mkdir -p /var/run/netns
 
-# Iterate over number of instances 
+# Iterate over number of instances
 # echo "Starting containers"
 for i in `seq 1 $2` ; do
 	__id=`docker run --detach $1 | cut -c1-12`
